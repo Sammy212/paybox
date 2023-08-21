830c9ff1c92f65932237d5422e1c098de5e5c87f
Sammy212 <samuelafo.212@gmail.com>
2023-08-21 12:08:45 +0100

    ReactDom Version Fix | Grid layout | Gobal style Update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css'
+import Dashboard from './components/Dashbaord/Dashboard';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
-        Paybox
+      <Sidebar/>
+      <Dashboard/>
     </div>
   );
 }
